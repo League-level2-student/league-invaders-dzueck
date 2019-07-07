a88c3ae12d6161a898066c2407690c8314ea6402
League Student <league@iMac-17.attlocal.net>
2019-07-07 15:56:18 -0700

    started

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,27 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	GamePanel gamePanel;
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	LeagueInvaders(){
+		frame = new JFrame();
+		
 
+		gamePanel = new GamePanel();
+	}
+	void setup() {
+		frame.add(gamePanel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.addKeyListener(gamePanel);
+	}
+	public static void main(String[] args) {
+		LeagueInvaders runner = new LeagueInvaders();
+		runner.setup();
+	}
 }
+//http://central.jointheleague.org/levels/Level2/InvadersOverview.html
